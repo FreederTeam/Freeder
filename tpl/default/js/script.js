f647b5c5f94e90f2cfd0a9e1baefbd2c9a4dedc1
Élie Michel <elie.michel@ens.fr>
2014-07-25 00:43:55 +0200

    Add ajax request for marking as read

--- tpl/default/js/script.js
+++ tpl/default/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 	svgeezy.init(false, 'png');
 });
+
+function ajax(target) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', target, false);
+	xhr.send(null);
+	return false;
+}
