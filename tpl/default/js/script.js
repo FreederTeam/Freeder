88c0ced107e659ca143e43e4c65d46f122e79bdd
Élie Michel <elie.michel@ens.fr>
2014-07-25 00:57:36 +0200

    Clean ajax request and remove article from view

--- tpl/default/js/script.js
+++ tpl/default/js/script.js
@@ -2,9 +2,17 @@ $(document).ready(function() {
 	svgeezy.init(false, 'png');
 });
 
-function ajax(target) {
+function ajax(caller, target) {
+	caller.innerHTML = "Non Lu";
+
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', target, false);
+	xhr.open('GET', target, true);
+	xhr.onload = function () {
+		if (xhr.status == 200) {
+			var article = caller.parentNode;
+			article.parentNode.removeChild(article);
+		}
+	};
 	xhr.send(null);
 	return false;
 }
