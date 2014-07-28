f371a35903be2a57a17e382a1c8199c5d8749112
Tom Canac <contact@tomcanac.com>
2014-07-28 17:08:57 +0200

    theme modifications are back \o/ the tag icon on medium screen size is not finished. Need mode finitions, but the main idea is here :)

--- tpl/default/js/script.js
+++ tpl/default/js/script.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
-	svgeezy.init(false, 'png');
-});
+// == Init
+	$(document).ready(function() {
+		svgeezy.init(false, 'png');
+	});
 
-function ajax(caller, target) {
-	caller.innerHTML = "Non Lu";
+// == Functions
+	function ajax(caller, target) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', target, true);
+		xhr.onload = function () {
+			if (xhr.status == 200) {
+				var article = caller.parentNode.parentNode;
+				article.parentNode.removeChild(article);
+			}
+		};
+		xhr.send(null);
+		return false;
+	}
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', target, true);
-	xhr.onload = function () {
-		if (xhr.status == 200) {
-			var article = caller.parentNode.parentNode;
-			article.parentNode.removeChild(article);
-		}
-	};
-	xhr.send(null);
-	return false;
-}
+// == Display Tags
+	$(".articleDisplayTagButton").click(function(){
+		$(this).siblings("div").slideToggle(200);
+	});
