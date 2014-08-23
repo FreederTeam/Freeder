975f75759a0bd404f52cc368186017b0fd44ca3b
Phyks <webmaster@phyks.me>
2014-08-23 15:11:15 +0200

    Refactor AJAX + tag button implemented
    * Use `$.get()` jQuery method for conveniency.
    * Implemented tag button in AJAX.

--- tpl/default/js/script.js
+++ tpl/default/js/script.js
@@ -5,16 +5,27 @@
 
 // == Functions
 	function ajax(caller, target) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', target, true);
-		xhr.onload = function () {
-			if (xhr.status == 200) {
+		$.get(target, function(data) {
+			if (data == 'OK') {
 				var article = caller.parentNode.parentNode;
 				article.parentNode.removeChild(article);
 			}
-		};
-		xhr.send(null);
+		});
 		return false;
+	}
+
+
+	function tag(caller, entry_id, tag_baselink) {
+		var tag_value = $('#newTag').val();
+		$.get('api/tags.php?entry='+entry_id+'&tag='+tag_value, function(data) {
+			if (data == 'OK') {
+				$('#newTag').val("");
+				var article = caller.parentNode.parentNode;
+				$('ul', $(article)).append('<li><a href="'+tag_baselink+tag_value+'">'+tag_value+'</a></li>');
+)
+
+			}
+		});
 	}
 
 // == Display Tags
