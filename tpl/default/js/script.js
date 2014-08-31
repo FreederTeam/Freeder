112e21a895b6db70b5ef902d0a5b93f8a90e0b1c
Tom Canac <contact@tomcanac.com>
2014-08-31 18:10:18 +0200

    wow, modalbox function written.

--- tpl/default/js/script.js
+++ tpl/default/js/script.js
@@ -74,3 +74,12 @@
 	$(".DisplayTagsButton").click(function(){
 		$(this).siblings(".ArticleTagsList").slideToggle(200);
 	});
+
+// == Modal Box
+	function fillModalbox(title, content, className) {
+		if (className != undefined) {
+			$("#JsModalbox").addClass(className);
+		}
+		$("#JsModalbox-h1").html(title);
+		$("#JsModalbox-p").html(content);
+	}
