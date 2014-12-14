37027984f43459d7f448760dccf17619d36258b4
Élie Michel <elie.michel@ens.fr>
2014-12-14 18:14:14 +0100

    Improve SimpleTpl

--- SimpleTpl.class.php
+++ SimpleTpl.class.php
@@ -64,6 +64,26 @@ class SimpleTpl {
 
 
 	/**
+	 * Load file content
+	 * Reset $error
+	 * @param $path: path to the file
+	 * @return file content
+	 */
+	public function load($view_path) {
+		$this->error = NULL;
+
+		$filename = 'tpl/'.$view_path.$this->view_extension;
+		$view = @file_get_contents($filename);
+		if ($view === FALSE) {
+			$this->error = 'File not found: '.$filename;
+			return FALSE;
+		}
+
+		return $view;
+	}
+
+
+	/**
 	 * Check whether the given path does not contain dangerous patterns
 	 * such as .. that would allow the user to explore server filesystem
 	 * Reset $error
@@ -81,6 +101,27 @@ class SimpleTpl {
 		}
 
 		return TRUE;
+	}
+
+
+	/**
+	 * Load view dependencies signaled by {include "foo"}
+	 * Reset $error
+	 * @param $url_prefix: prefix to add to relative URLs
+	 * @param $view: view content to process
+	 * @return full view
+	 */
+	public function load_included($url_prefix, $view) {
+		$this->error = NULL;
+
+		preg_replace_callback('/\\{include="(([^}]|\\\\\\})*)"\\}/', function ($matches) {
+			$view = $this->load($matches[1]);
+			var_dump(self::clean_path($matches[1]));
+			if (!is_null($this->error)) return "{}";
+			return $view;
+		}, $view);
+
+		return $view;
 	}
 
 
@@ -114,16 +155,12 @@ class SimpleTpl {
 		if (!is_null($this->error)) return FALSE;
 
 		// Load raw view from file
-		$filename = 'tpl/'.$view_path.$this->view_extension;
-		$view = @file_get_contents($filename);
-		if ($view === FALSE) {
-			$this->error = 'File not found: '.$filename;
-			return FALSE;
-		}
+		$view = $this->load($view_path);
+		if (!is_null($this->error)) return FALSE;
 
-		// Load included templates (todo)
-		//$view = $this->load_included($view);
-		//if (!is_null($this->error)) return FALSE;
+		// Load included templates
+		$view = $this->load_included($view);
+		if (!is_null($this->error)) return FALSE;
 
 		// Rewrite URLs
 		$view = $this->rewrite_urls($view);
@@ -137,6 +174,34 @@ class SimpleTpl {
 		echo($view);
 
 		return TRUE;
+	}
+
+
+	/**
+	 * Clean path by removing useless .. and .
+	 * @param $path: Path to clean
+	 * @return clean path
+	 */
+	public static function clean_path($path) {
+		return preg_replace_callback(
+			'#^((?:\\.\\./)*)(.*)#', 
+			function($matches){
+				$prefix = $matches[1]; // Leading ../
+				$path = $matches[2];   // Remaining path
+
+				// Remove ./
+				$path = preg_replace('#/\\./#', '/', $path);
+				$path = preg_replace('#(^\\./)|(/\\.$)#', '', $path);
+
+				// Remove ../
+				while (preg_match('#(^|/)\\.\\.(/|$)#', $path)) {
+					$path = preg_replace('#(^|/)[^/]*/\\.\\.(?:/|$)#', '$1', $path);
+				}
+
+				return $prefix.$path;
+			},
+			$path
+		);
 	}
 
 }
