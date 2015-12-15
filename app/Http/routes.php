7e30738fe8166b6b44a0b651109349a3855b57b6
Phyks (Lucas Verney) <phyks@phyks.me>
2015-12-15 19:01:17 +0100

    Add an index to the API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,15 +10,27 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-$app->bind('League\Fractal\Manager', function($app) {
-	$fractal = new \League\Fractal\Manager;
-	$serializer = new \League\Fractal\Serializer\JsonApiSerializer('/api/v1/');
-	$fractal->setSerializer($serializer);
-	return $fractal;
+$app->bind('League\Fractal\Manager', function ($app) {
+    $fractal = new \League\Fractal\Manager;
+    $serializer = new \League\Fractal\Serializer\JsonApiSerializer('/api/v1');
+    $fractal->setSerializer($serializer);
+    return $fractal;
 });
 
 $app->get('/', function () use ($app) {
     return $app->welcome();
+});
+
+
+$app->get('/api', function () {
+    return redirect("/api/v1/");
+});
+
+$app->get('/api/v1/', function () {
+    return [
+        "feeds" => "/api/v1/feeds",
+        "entries" => "/api/v1/entries"
+    ];
 });
 
 $app->get('/api/v1/feeds', 'FeedController@index');
